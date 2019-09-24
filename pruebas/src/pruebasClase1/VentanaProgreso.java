997b5f46a9b5e79364db8da94ea24668678e1668
javitxu.gonzalez <javitxu.gonzalez@E110L20.opendeusto.es>
2019-09-24 10:52:37 +0200

    cambios

--- pruebas/src/pruebasClase1/VentanaProgreso.java
+++ pruebas/src/pruebasClase1/VentanaProgreso.java
@@ -1,27 +1,65 @@
 package pruebasClase1;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
-public class VentanaProgreso extends JFrame {
+public class VentanaProgreso extends JFrame{
+	
+	
+	public VentanaProgreso() {
+	setTitle("Ejemplo de barra de progreso");
+	setSize(640,480);
+	setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	
+	JPanel pButton= new JPanel();
 
+	JButton button = new JButton("Click me!");
+	add(pButton,BorderLayout.SOUTH);
+	pButton.add(button);
+	
+	JProgressBar progressBar=new JProgressBar(0,10000);
+	add(progressBar,BorderLayout.NORTH);
+	
+	button.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			for(long i=0;i<Long.MAX_VALUE;i++) {
+				
+				System.out.println(i);
+			}
+			
+			
+			
+		}
+		
+	});
+		
+	
+		
+	
+	
+	}
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			
 		VentanaProgreso v1 = new VentanaProgreso();
-		v1.setBounds(0, 0, 600, 400);
 		v1.setVisible(true);
-		JPanel panel1 = new JPanel();
-		JProgressBar progressBar = new JProgressBar();
-		v1.add(panel1);
-
-		panel1.add(progressBar);
+		
 		
 	}
 
+		
 }
