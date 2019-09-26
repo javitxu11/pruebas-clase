61e710990d17cb294eb3abeb45fbed25901f5d5a
javitxu.gonzalez <javitxu.gonzalez@E110L20.opendeusto.es>
2019-09-26 16:20:40 +0200

    update

--- pruebas/src/pruebasClase2/Main.java
+++ pruebas/src/pruebasClase2/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=3;
-		Integer b=3;
+		Integer b=new Integer(3);
+		Integer c=5;
+		
 		System.out.println(a+b);
 		
 	}
