c6fccb3ffffb30889ea718ed67db51f83ae071f4
javitxu.gonzalez <javitxu.gonzalez@E110L20.opendeusto.es>
2019-09-26 16:20:30 +0200

    clase de pruebas varias

---
+++ pruebas/src/pruebasClase2/Main.java
@@ -0,0 +1,13 @@
+package pruebasClase2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+		Integer b=3;
+		System.out.println(a+b);
+		
+	}
+
+}
