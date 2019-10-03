42f0bfd6400319bef4e5aef2e79fab828ae4427a
javitxu.gonzalez <javitxu.gonzalez@E110L20.opendeusto.es>
2019-10-03 15:56:39 +0200

    cambios en calculadora test

--- pruebas/test/pruebasClase2/CalculadoraTest.java
+++ pruebas/test/pruebasClase2/CalculadoraTest.java
@@ -2,25 +2,36 @@ package pruebasClase2;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculadoraTest {
 
+	private Calculadora c;
+	
+	@Before
+	public void setUp() {
+		 c = new Calculadora();
+	}
+	
 	@Test
 	public void testSumar() {
-		Calculadora c = new Calculadora();
 		int resultado = c.sumar(5, 3);
 		assertEquals(8,resultado);
-		
 	}
-
 	
 	@Test
 	public void testRestar() {
-		Calculadora c= new Calculadora();
-		assertEquals(3,c.restar(10, 7));
-		
-		
+		assertEquals(3,c.restar(10, 7));	
 	}
 	
+	@Test
+	public void testMultiplicar() {
+		assertEquals(4,c.multiplicar(2, 2));
+	}
+	
+	@Test
+	public void testCuadrado() {
+		assertEquals(4.0,c.cuadrado(2), 0.0);
+	}
 }
