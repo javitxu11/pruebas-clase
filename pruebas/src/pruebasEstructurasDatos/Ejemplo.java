d9c0be4bb9b28375c371f91b3328920230bae822
javit <javitxu.gonzalez@opendeusto.es>
2019-11-19 10:33:48 +0100

    udtgcc

--- pruebas/src/pruebasEstructurasDatos/Ejemplo.java
+++ pruebas/src/pruebasEstructurasDatos/Ejemplo.java
@@ -8,7 +8,7 @@ public class Ejemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-		List<Persona> personas = new ArrayList<Persona>();
+		List<Persona> personas = new ArrayList<>();
 		Persona p = new Persona("Pepe","Sanchez",35);
 		personas.add(p);
 		Persona j= new Persona("John","Dominguez",21);
